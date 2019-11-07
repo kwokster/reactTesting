17d0890459406c79784e480ec7124b1423ffd3dd
Jon Kwok <jkwokcl@gmail.com>
2019-11-07 16:18:59 -0500

    moxios api call testing

--- src/__tests__/intergrations.test.js
+++ src/__tests__/intergrations.test.js
@@ -2,11 +2,37 @@
 
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
+//moxies makes fake requests. No network request is actually being called.
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
+beforeEach(() => {
+   //tells moxios to intercept any axios call issued
+   moxios.install();
+   moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+      status: 200,
+      response: [{ name: 'Fetched 1'}, { name: 'Fetched 2'}]
+   });
+});
+
+afterEach(() => {
+   moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', (done) => {
    // Attempt to render the entire app
+   const wrapper = mount(<Root><App /></Root>);
    // Find the 'fetchComments' button and click it
-   // Expect to find a list of comments 
+   wrapper.find('.fetch-comments').simulate('click');
+   // Introduce a tiny little pause
+   // setTimeout(() => {
+      moxios.wait(() => { 
+         wrapper.update();
+         // Expect to find a list of comments 
+         expect(wrapper.find('li').length).toEqual(2);
+         done(); //this is the signal to say there is nothing else to test
+         wrapper.unmount(); 
+      });
+   // }, 100);
 });
